53b9735336d049277b742f9dce3d0c8c17ddac49
Carol00000 <caroloppuer03@gmail.com>
2025-11-24 22:51:12 +0800

    Fix Devise permitted params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,8 +15,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) {|user| user.permit(:first_name, :last_name, :balance, :email, :password, :password_confirmation)}
-    devise_parameter_sanitizer.permit(:account_update) {|user| user.permit(:first_name, :last_name, :balance, :email, :password, :current_password)}
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
 
   def after_sign_in_path_for(resource)
